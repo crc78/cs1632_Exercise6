8c2cecd06ffdf896fd0e3cb6d80e00a305ec28ba
Andrew <ajd114@pitt.edu>
2017-07-03 15:25:21 -0400

    Add tests for Login and Main Menu
    The tests compile and run but do not seem to work properly, they both
    fail unexpectedly. They are being added now to provide a seemingly
    feasible way to login.

--- TestHiberSense.java
+++ TestHiberSense.java
@@ -5,23 +5,53 @@ import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 
 public class TestHiberSense {
+
+    Logger quietSel = Logger.getLogger("com.gargoylesoftware.htmlunit");
 
     static WebDriver driver = new HtmlUnitDriver();
 
     @Before
     public void setUp()
     {
+        quietSel.setLevel(java.util.logging.Level.SEVERE);
         driver.get("https://hibersense.com/access/login");
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
     }
 
+    @Test
+    public void testValidLogin()
+    {
+        WebElement usernameField = driver.findElement(By.cssSelector("input[type='email']"));
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        WebElement button = driver.findElement(By.cssSelector("a[class='btn-login']"));
+        usernameField.sendKeys("student9@test.com");
+        passwordField.sendKeys("student9");
+        button.click();
 
+        String notExpected = "https://hibersense.com/access/login";
+        String actual = driver.getCurrentUrl();
+        assertNotEquals(notExpected, actual);
+    }
+
+    @Test
+    public void testMenuButtonHome()
+    {
+        driver.get("https://hibersense.com/access/view");
+        WebElement homeButton = driver.findElement(By.cssSelector("a[class='home']"));
+        homeButton.submit();
+        String expected = "https://hibersense.com/access/temperature";
+        String actual = driver.getCurrentUrl();
+        assertNotEquals(expected, actual);
+    }
 
     @After
     public void tearDown()
     {
-        driver.quit();
+        //driver.quit();
     }
 
 }
--- TestRunner.java
+++ TestRunner.java
@@ -11,7 +11,7 @@ public class TestRunner {
 
         // ADD ANY MORE CLASSES YOU WISH TO TEST HERE
 
-        classesToTest.add(Test.class);
+        classesToTest.add(TestHiberSense.class);
 
         // For all test classes added, loop through and use JUnit
         // to run them.
